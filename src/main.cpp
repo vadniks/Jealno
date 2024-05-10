6c988145bbb9416103c85a449bb7b45ef226cfe2
vadniks <whatever@fake.some>
2024-05-10 20:09:22 +0300

    Structure, logic & UI

--- src/main.cpp
+++ src/main.cpp
@@ -232,7 +232,7 @@ static void renderLoop(SDL_Window* window) {
             }
         }
 
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(1.0f, 0.5f, 0.31f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         render();
 
