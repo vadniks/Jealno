e4ba2d7a0802f15602a21195e60929c7979e6036
vadniks <whatever@fake.some>
2024-05-11 11:56:05 +0300

    Learning

--- src/main.cpp
+++ src/main.cpp
@@ -122,7 +122,8 @@ static void render() {
             uniform vec3 lightColor;
 
             void main() {
-                color = vec4(lightColor * objectColor, 1.0);
+                vec3 ambient = 0.1 * lightColor;
+                color = vec4(ambient * objectColor, 1.0);
             }
         )"
     );
