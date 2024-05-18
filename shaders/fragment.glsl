58b73ae63b514f3212889555520630a4e1f7ce2a
vadniks <whatever@fake.some>
2024-05-18 17:37:14 +0300

    Learning

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,6 +7,15 @@ out vec4 FragColor;
 
 uniform sampler2D texture0;
 
+float near = 0.1;
+float far = 100.0;
+
+float LinearizeDepth(float depth) {
+    float z = depth * 2.0 - 1.0;
+    return (2.0 * near * far) / (far + near - z * (far - near));
+}
+
 void main() {
-    FragColor = texture(texture0, TexCoords);
+
+    FragColor =  vec4(vec3(LinearizeDepth(gl_FragCoord.z) / far), 1.0); //texture(texture0, TexCoords);
 }
